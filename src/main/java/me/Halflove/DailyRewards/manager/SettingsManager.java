c23287aad0308a98b3bcc1348cba3f977c33b0b8
masterHAWK99 <maciej.pochron99@gmail.com>
2021-12-16 17:34:06 +0100

    Add methods to loading and saving mapped configs

--- src/main/java/me/Halflove/DailyRewards/manager/SettingsManager.java
+++ src/main/java/me/Halflove/DailyRewards/manager/SettingsManager.java
@@ -1,11 +1,19 @@
 package me.Halflove.DailyRewards.manager;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import me.Halflove.DailyRewards.Main;
 import org.bukkit.plugin.Plugin;
 
 public class SettingsManager {
+
+    private final ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory()).configure(JsonParser.Feature.IGNORE_UNDEFINED, true);
 
     private static Plugin plugin;
 
@@ -31,6 +39,39 @@ public class SettingsManager {
 
     public static Config getConfig() {
         return config;
+    }
+
+    /**
+     * Maps given yml file content into given Java type.
+     *
+     * @return object with mapped fields or new object with default values if file doesn't exist
+     */
+    private <T> T loadConfig(File file, Class<T> clazz) {
+        if (file.exists()) {
+            try {
+                return objectMapper.readValue(file, clazz);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                return clazz.getDeclaredConstructor().newInstance();
+            } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Serializes given Java type into yml file.
+     */
+    private void saveConfig(File file, Object object) {
+        try {
+            objectMapper.writeValue(file, object);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setup() {
